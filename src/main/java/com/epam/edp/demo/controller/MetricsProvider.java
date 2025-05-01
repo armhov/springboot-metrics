fb4a835c85757f0140d8995b5c6b5909ae4b3705
armhov <armhov24@gmail.com>
2025-05-01 17:41:59 +0400

    fix 1

--- src/main/java/com/epam/edp/demo/controller/MetricsProvider.java
+++ src/main/java/com/epam/edp/demo/controller/MetricsProvider.java
@@ -3,7 +3,6 @@ package com.epam.edp.demo.controller;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.lang.management.ManagementFactory;
@@ -16,22 +15,20 @@ import java.lang.management.MemoryUsage;
 @Component
 public class MetricsProvider {
     private final MeterRegistry meterRegistry;
-    private final String studentName;
 
-    public MetricsProvider(MeterRegistry meterRegistry, @Value("${student.name:default}") String studentName) {
+    public MetricsProvider(MeterRegistry meterRegistry) {
         this.meterRegistry = meterRegistry;
-        this.studentName = studentName;
     }
 
     @PostConstruct
     public void registerCustomMetrics() {
         MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
 
-        Gauge.builder("jvm_memory_usage_" + studentName, memoryMXBean, memory -> {
+        Gauge.builder("jvm_memory_usage_647f9884", memoryMXBean, memory -> {
                     MemoryUsage heapMemoryUsage = memory.getHeapMemoryUsage();
                     return (double) heapMemoryUsage.getUsed() / heapMemoryUsage.getMax();
                 })
-                .description("JVM memory usage with student name")
+                .description("JVM memory usage with fixed metric name")
                 .register(meterRegistry);
     }
 }
